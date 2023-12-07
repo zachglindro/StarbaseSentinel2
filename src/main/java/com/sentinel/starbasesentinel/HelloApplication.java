7c54d7bcbf43aee8667aab1f9f8d7647e2325f7a
zdmgg <zachdwaynemglindro@gmail.com>
2023-12-07 22:09:11 +0800

    Change text

--- src/main/java/com/sentinel/starbasesentinel/HelloApplication.java
+++ src/main/java/com/sentinel/starbasesentinel/HelloApplication.java
@@ -11,8 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Starbase Sentinel");
         stage.setScene(scene);
         stage.show();
     }

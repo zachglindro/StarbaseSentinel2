5da20f4cf4009b6e6f860bc669913ba3266072e5
zdmgg <zachdwaynemglindro@gmail.com>
2023-12-15 20:47:55 +0800

    Add more methods for translate grid to raw and back

--- src/main/java/com/sentinel/starbasesentinel/levels/Grid.java
+++ src/main/java/com/sentinel/starbasesentinel/levels/Grid.java
@@ -6,11 +6,19 @@ public class Grid {
     // 1280x720 is converted to 26x15 grid
     final static int cellSize = 48; // Cell size in pixels
 
-    public static Point2D translate(double x, double y) {
+    public static Point2D translateToCoords(double x, double y) {
         return new Point2D(x * cellSize + 24, y * cellSize + 24);
     }
 
-    public static double translate(double x) {
+    public static double translateToCoords(double x) {
         return x * cellSize + 24;
+    }
+
+    public static Point2D translateToGrid(double x, double y) {
+        return new Point2D((x - 24) / cellSize, (y - 24) / cellSize);
+    }
+
+    public static double translateToGrid(double x) {
+        return (x - 24) / cellSize;
     }
 }
